2a1e982031d182a62af3685bba2a4308644db79c
Bhaveshwagh04 <121219011+Bhaveshwagh04@users.noreply.github.com>
2024-05-27 00:13:17 +0530

    app added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Bhaveshwagh04",
+    author_email="bhaveshwaghbw.49@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
